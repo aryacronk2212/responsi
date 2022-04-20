5fb03f6764e2e7160ea989b9c2f703c98a369b36
aryacronk2212 <101683832+aryacronk2212@users.noreply.github.com>
2022-04-20 10:11:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiArya
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Karyawan paijo = new Karyawan("1444444", "Paijo", 2000000);
+            Karyawan jono = new Karyawan("1555555", "Jono", 3000000);
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", paijo.NIK, paijo.Nama, paijo.GajiBulanan);
+            Console.WriteLine("2    {0}/{1}         {2}", jono.NIK, jono.Nama, jono.GajiBulanan);
+
+            Console.WriteLine();
+            Console.WriteLine("Naikkan gaji sebesar 10%");
+            Console.WriteLine();
+
+            paijo.NaikkanGaji(10);
+            jono.NaikkanGaji(10);
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", paijo.NIK, paijo.Nama, paijo.GajiBulanan);
+            Console.WriteLine("2    {0}/{1}         {2}", jono.NIK, jono.Nama, jono.GajiBulanan);
+        }
+    }
+}
